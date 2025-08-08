9496b80f6dcb27a247fba2760d04cfcf299c2b7d
Samuele Lombardi <59124097+Dieal@users.noreply.github.com>
2025-08-08 16:42:29 +0200

    fix: FileSystemNotFoundException on some systems when loading .zip file
    Fixed it by opening the zip data stream and by creating a temporary
    file to read from

--- src/main/java/com/nnamo/services/StaticGtfsService.java
+++ src/main/java/com/nnamo/services/StaticGtfsService.java
@@ -4,7 +4,11 @@ import org.onebusaway.gtfs.impl.GtfsRelationalDaoImpl;
 import org.onebusaway.gtfs.serialization.GtfsReader;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.file.Paths;
@@ -26,8 +30,24 @@ public class StaticGtfsService {
         reader.setEntityStore(this.store);
 
         URL staticFeedURL = getClass().getResource("/rome_static_gtfs.zip");
-        File staticFeedFile = Paths.get(staticFeedURL.toURI()).toFile();
-        reader.setInputLocation(staticFeedFile);
+        if (staticFeedURL == null) {
+            throw new FileNotFoundException("GTFS zip not found in resources.");
+        }
+
+        // Reads the zip file from the Jar Resource and creates a temporary file
+        // (This fixes FileSystemNotFoundException on some systems)
+        InputStream in = staticFeedURL.openStream();
+        File tempFile = File.createTempFile("rome_static_gtfs", ".zip");
+        tempFile.deleteOnExit();
+        try (OutputStream out = new FileOutputStream(tempFile)) {
+            byte[] buffer = new byte[8192];
+            int bytesRead;
+            while ((bytesRead = in.read(buffer)) != -1) {
+                out.write(buffer, 0, bytesRead);
+            }
+        }
+
+        reader.setInputLocation(tempFile);
         reader.run();
         System.out.println("Dati GTFS statici caricati con successo.");
     }
