883009c449ff1801c6a4a7df5b72a4c9d4ea3ef4
Riccardo <riccardo.finocchiaro5@gmail.com>
2025-09-09 18:11:16 +0200

    feat: new test

--- src/test/java/com/nnamo/view/customcomponents/CustomPreferButtonTest.java
+++ src/test/java/com/nnamo/view/customcomponents/CustomPreferButtonTest.java
@@ -1,19 +1,37 @@
 package com.nnamo.view.customcomponents;
 
+import com.nnamo.enums.DataType;
 import com.nnamo.utils.CustomColor;
 import junit.framework.TestCase;
 public class CustomPreferButtonTest extends TestCase {
 
     CustomPreferButton button = new CustomPreferButton();
-    public void testUpdateTrue() {
+
+    public void testUpdateTrueOnStop() {
+        button.setItemName("fermata");
         button.update(true);
-        assertEquals("<html><p>Rimuovi</p><p>Test Item</p><p>dai Preferiti</p></html>", button.getText());
+        assertEquals("Rimuovi fermata dai Preferiti", button.getText());
         assertEquals(CustomColor.RED, button.getBackground());
     }
 
-    public void testUpdateFalse() {
+    public void testUpdateFalseOnStop() {
+        button.setItemName("fermata");
         button.update(false);
-        assertEquals("<html><p>Aggiungi</p><p>Test Item</p><p>ai Preferiti</p></html>", button.getText());
+        assertEquals("Aggiungi fermata ai Preferiti", button.getText());
+        assertEquals(CustomColor.GREEN, button.getBackground());
+    }
+
+    public void testUpdateTrueOnRoute() {
+        button.setItemName("linea");
+        button.update(true);
+        assertEquals("Rimuovi linea dai Preferiti", button.getText());
+        assertEquals(CustomColor.RED, button.getBackground());
+    }
+
+    public void testUpdateFalseOnRoute() {
+        button.setItemName("linea");
+        button.update(false);
+        assertEquals("Aggiungi linea ai Preferiti", button.getText());
         assertEquals(CustomColor.GREEN, button.getBackground());
     }
 }
